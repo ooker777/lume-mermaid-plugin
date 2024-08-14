f7116f50b2d655dc5f6fe91318683ebd9a8fb1cd
ooker <ganuongphap@gmail.com>
2024-08-14 22:17:36 +0700

    chore: switch to js to publsih to jsr

--- mod.js
+++ mod.js
@@ -1,11 +1,8 @@
-import Site, { Plugin } from "lume/core/site.ts";
-
 const mermaidScript = `
 import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';
 mermaid.initialize({ startOnLoad: true });`
-
-export default function (): Plugin {
-  return (site: Site) => {
+export default function (){
+  return (site) => {
     site.process([".html"], (pages) => {
       for (const page of pages) {
         const document = page.document;
@@ -14,14 +11,14 @@ export default function (): Plugin {
         const elements = document.querySelectorAll("pre code.language-mermaid");
 
         if (elements.length) {
-          const scriptElement = document.createElement("script") as HTMLScriptElement;
+          const scriptElement = document.createElement("script");
           scriptElement.setAttribute("type", "module")
           scriptElement.textContent = mermaidScript;
           document.head.appendChild(scriptElement);
         }
 
         for (const element of elements) {
-          const pre = element.parentElement!;
+          const pre = element.parentElement;
           pre.textContent = element.textContent;
           pre.classList.add("mermaid");
         }
--- test/_config.ts
+++ test/_config.ts
@@ -1,6 +1,6 @@
 import lume from "lume/mod.ts";
-import mermaid from "../mod.ts";
-// import mermaid from "https://deno.land/x/lume_mermaid_plugin/mod.ts";
+import mermaid from "../mod.js";
+// import mermaid from "https://deno.land/x/lume_mermaid_plugin/mod.js";
 
 const site = lume();
 site.use(mermaid())
